329fd241317962bea087dd653ed465a87f2f0acf
pholmq <pholmq@gmail.com>
2022-12-30 12:20:56 +0100

    materials

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,40 @@
 import { Canvas } from "@react-three/fiber";
 import { describe } from "node:test";
 import AnimatedBox from "../components/AnimatedBox";
-import { OrbitControls, Stats } from "@react-three/drei";
+import { OrbitControls, Stats, useTexture } from "@react-three/drei";
+
+
+const TexturedSpheres = () => {
+  const map = useTexture("./textures/metal_plate_diff_1k.jpg");
+  const displacementMap = useTexture("./textures/metal_plate_disp_1k.png");
+  const normalMap = useTexture("./textures/metal_plate_nor_gl_1k.jpg");
+  const roughnessMap = useTexture("./textures/metal_plate_rough_1k.jpg");
+
+  return (
+    <>
+    <mesh scale={[0.5, 0.5, 0.5]} position={[-1, 0, 0]}>
+      <sphereGeometry />
+      <meshStandardMaterial map={map}/>
+    </mesh>
+    <mesh scale={[0.5, 0.5, 0.5]} position={[0, 0, 0]}>
+      <sphereGeometry />
+      <meshStandardMaterial map={map} normalMap={normalMap} roughnessMap={roughnessMap}/>
+    </mesh>
+    <mesh scale={[0.5, 0.5, 0.5]} position={[1, 0, 0]}>
+      <sphereGeometry args={[1, 300, 300]} />
+      <meshStandardMaterial 
+      map={map} 
+      normalMap={normalMap} 
+      roughnessMap={roughnessMap} 
+      displacementMap={displacementMap}
+      displacementScale={0.1}
+      />
+    </mesh>
+
+
+    </>
+  )
+}
 
 export default function Home() {
 
@@ -23,12 +56,13 @@ export default function Home() {
       </>
        : null }
       <OrbitControls/>
-      <ambientLight intensity={0.1}/>
-      <directionalLight position={[0,0,5]}/>
-      <mesh >
+      <ambientLight intensity={0.3}/>
+      <directionalLight position={[0,5,5]}/>
+      <TexturedSpheres/>
+      {/* <mesh >
       <coneGeometry/>
       <meshStandardMaterial color="blue" wireframe/>
-    </mesh>
+    </mesh> */}
       {/* <AnimatedBox/> */}
     </Canvas>
     </div>
